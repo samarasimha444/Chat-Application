26c9d2b674f3be278163697152b7643cef482916
samarasimhareddy <samarasimha444@gmail.com>
2025-10-27 16:01:56 +0530

    added confirm password logic

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
 let Home=()=>{
     return(
         <div>
             <h1>Home Page</h1>
+            <Link to='/login'>login</Link>
+            <br/>
+            <Link to='/signup'>signup</Link>
         </div>
     )
 }
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -4,20 +4,27 @@ let SignUp=()=>{
     let[username,setUsername]=useState("");
     let[password,setPassword]=useState("");
     let[confirmPassword,setConfirmPassword]=useState("");
+    let[match,setMatch]=useState(false);
+    let passWordMatch=()=>{
+       password===confirmPassword ? setMatch("matched"):setMatch("not matched");
+        }
+
     
     return(
         <div>
             <h1>Sign Up Page</h1>
             <input type="text" placeholder="Username" value={username} onChange={(e)=>setUsername(e.target.value)}/>
-            <div>{username}</div>
+            
             <br/>
             <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-            <div>{password}</div>
+           
             <br/>
             <input type="password" placeholder="Confirm Password" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}/>
             <div>{confirmPassword}</div>
             <br/>
-            <button type="submit">Sign Up</button>
+            <button type="submit" onClick={passWordMatch}>Sign Up</button>
+            {match ? <div>{match}</div>:<div>{match}</div>}
+            
             <button>
                 <Link to="/login">I already have an account</Link>
             </button>
